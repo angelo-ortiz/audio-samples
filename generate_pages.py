006de3c2db365f23b99a44e5ea7f46e4dcff053d
Angelo Ortiz <aortizt@meta.com>
2025-10-28 16:00:31 +0000

    Add HiFi-GAN at different steps

--- generate_pages.py
+++ generate_pages.py
@@ -2,6 +2,7 @@ from pathlib import Path
 
 from babblerl.io import read_manifest
 from babblerl.constants import ALIGNMENTS_ROOT, AUDIO_ROOT
+from babblerl.metrics import _normalize
 from babblerl.utils import get_all_transcriptions, read_transcription_and_metrics
 from jinja2 import Environment, FileSystemLoader
 from pydub import AudioSegment
@@ -17,8 +18,10 @@ DST_DATA_DIR = Path('data')
 TEMPLATE_DIR = Path('templates')
 MENU_PAGES = [
     {'src_name': 'pre_trained_sparc/multi', 'dst_name': 'sparc_multi', 'title': 'SPARC-multi', 'filename': 'sparc_multi.html'},
-    {'src_name': 'pre_trained_sparc/en+', 'dst_name': 'sparc_en_plus', 'title': 'SPARC-en+', 'filename': 'sparc_en_plus.html'},
-    {'src_name': 'trained_hifi_gan', 'dst_name': 'hifi_gan', 'title': 'HiFi-GAN', 'filename': 'hifi_gan.html'},
+    # {'src_name': 'pre_trained_sparc/en+', 'dst_name': 'sparc_en_plus', 'title': 'SPARC-en+', 'filename': 'sparc_en_plus.html'},
+    {'src_name': 'hifi_gan/train-50h_snr35_c10_s132k', 'dst_name': 'hifi_gan_132k', 'title': 'HiFi-GAN (132k/500k)', 'filename': 'hifi_gan_132k.html'},
+    {'src_name': 'hifi_gan/train-50h_snr35_c10_s310k', 'dst_name': 'hifi_gan_310k', 'title': 'HiFi-GAN (310k/500k)', 'filename': 'hifi_gan_310k.html'},
+    {'src_name': 'hifi_gan/train-50h_snr35_c10_s500k', 'dst_name': 'hifi_gan_500k', 'title': 'HiFi-GAN (500k/500k)', 'filename': 'hifi_gan_500k.html'},
 ]
 MAX_ROWS_PER_LANGUAGE_BEST = 3 # Limit the number of audio samples displayed per language
 
@@ -32,10 +35,11 @@ def remove_text_within_parentheses(text: str) -> str:
     Returns:
         The input string with the text within parentheses removed.
     """
-    if (beg := text.find('(')) != -1 and text.find(')') > beg:
+    if (beg := text.find('（')) != -1:
         return text[:beg].strip()
-    else:
-        return text
+    elif (beg := text.find('(')) != -1:
+        return text[:beg].strip()
+    return text
 
 
 def format_float_str(value: float | str, precision: int = 2, to_percent: bool = False) -> str:
@@ -52,7 +56,7 @@ def format_float_str(value: float | str, precision: int = 2, to_percent: bool = 
         value *= 100
     return f"{value:.{precision}f}"
 
-def get_language_data(src_page: str, dst_page: str, lang: str) -> dict[str, str | int | list[str]] | None:
+def get_language_data(src_page: str, dst_page: str, lang: str, selected_stems: list[str] | None = None):
     """Generates data for a specific language to be used in the HTML page.
 
     Args:
@@ -75,16 +79,23 @@ def get_language_data(src_page: str, dst_page: str, lang: str) -> dict[str, str 
     syn_trans = read_transcription_and_metrics(SRC_DATA_DIR / src_page / SPLIT / TRANSCRIPTION_DIR / f'{lang}.csv') # TODO: adapt here with another for acoustic metrics
 
     gt_ctc_trans = read_transcription_and_metrics(ALIGNMENTS_ROOT / SPLIT / CTC_TRANSCRIPTION_DIR / f'{lang}.csv')
-    syn_ctc_trans = read_transcription_and_metrics(SRC_DATA_DIR / src_page / SPLIT / CTC_TRANSCRIPTION_DIR / f'{lang}.csv')
+    if (ctc_trans_file := SRC_DATA_DIR / src_page / SPLIT / CTC_TRANSCRIPTION_DIR / f'{lang}.csv').exists():
+        syn_ctc_trans = read_transcription_and_metrics(ctc_trans_file)
+    else:
+        syn_ctc_trans = {stem: {} for stem in gt_ctc_trans}
 
-    syn_trans_items = sorted(syn_trans.items(), key=lambda x: x[1].get('wer', 0))  # Sort by WER
-    selected_stems = [item[0] for item in syn_trans_items[:MAX_ROWS_PER_LANGUAGE_BEST]]
-    selected_stems += [item[0] for item in syn_trans_items[-MAX_ROWS_PER_LANGUAGE_BEST:]]
+    if selected_stems is None:
+        syn_trans_items = sorted(syn_trans.items(), key=lambda x: x[1].get('wer', 0))  # Sort by WER
+        selected_stems = [item[0] for item in syn_trans_items[:MAX_ROWS_PER_LANGUAGE_BEST]]
+        selected_stems += [item[0] for item in syn_trans_items[-MAX_ROWS_PER_LANGUAGE_BEST:]]
 
     # Read the original transcriptions
     orig_transcriptions: dict[str, str] = get_all_transcriptions(AUDIO_ROOT / lang, return_header=False)  # type: ignore
     if lang == 'nan-tw':
-        orig_transcriptions = {stem: remove_text_within_parentheses(trans) for stem, trans in orig_transcriptions.items()}
+        orig_transcriptions = {
+            stem: _normalize(remove_text_within_parentheses(trans), remove_spaces=False) 
+            for stem, trans in orig_transcriptions.items()
+        }
 
     src_syn_audios_dir = SRC_DATA_DIR / src_page / SPLIT / 'audios' / lang
     dst_gt_audios_dir = DST_DATA_DIR / dst_page / lang / 'ground_truth'
@@ -97,9 +108,9 @@ def get_language_data(src_page: str, dst_page: str, lang: str) -> dict[str, str 
         
         # Convert WAV to MP3 and copy to destination directories
         gt_audio_paths.append(dst_gt_audios_dir / f'{stem}.mp3')
-        AudioSegment.from_wav(manifest[stem][0]).export(gt_audio_paths[-1], format='mp3')
+        _ = AudioSegment.from_wav(manifest[stem][0]).export(gt_audio_paths[-1], format='mp3')
         syn_audio_paths.append(dst_syn_audios_dir / f'{stem}.mp3')
-        AudioSegment.from_wav(src_syn_audios_dir / f'{stem}.wav').export(syn_audio_paths[-1], format='mp3')
+        _ = AudioSegment.from_wav(src_syn_audios_dir / f'{stem}.wav').export(syn_audio_paths[-1], format='mp3')
 
     return {
         'name': lang,
@@ -107,29 +118,36 @@ def get_language_data(src_page: str, dst_page: str, lang: str) -> dict[str, str 
         'ground_truth_audio': [f.relative_to(OUTPUT_DIR) for f in gt_audio_paths],
         'synthesised_audio': [f.relative_to(OUTPUT_DIR) for f in syn_audio_paths],
         'original_transcriptions': [orig_transcriptions[stem] for stem in selected_stems],
-        'ground_truth_transcriptions': [[trans[stem].get('transcription', '') for stem in selected_stems] for trans in [gt_trans, gt_ctc_trans]],
-        'synthesised_transcriptions': [[trans[stem].get('transcription', '') for stem in selected_stems] for trans in [syn_trans, syn_ctc_trans]],
+        'ground_truth_transcriptions': [[trans[stem].get('transcription', '?') for stem in selected_stems] for trans in [gt_trans, gt_ctc_trans]],
+        'synthesised_transcriptions': [[trans[stem].get('transcription', '?') for stem in selected_stems] for trans in [syn_trans, syn_ctc_trans]],
         'cer_ground_truth': [[format_float_str(trans[stem].get('cer', ''), to_percent=True) for stem in selected_stems] for trans in [gt_trans, gt_ctc_trans]],
         'cer_synthesised': [[format_float_str(trans[stem].get('cer', ''), to_percent=True) for stem in selected_stems] for trans in [syn_trans, syn_ctc_trans]],
         'wer_ground_truth': [[format_float_str(trans[stem].get('wer', ''), to_percent=True) for stem in selected_stems] for trans in [gt_trans, gt_ctc_trans]],
         'wer_synthesised': [[format_float_str(trans[stem].get('wer', ''), to_percent=True) for stem in selected_stems] for trans in [syn_trans, syn_ctc_trans]],
         'mcd': [format_float_str(syn_trans[stem].get('mcd', ''), to_percent=False) for stem in selected_stems],
         'f0': [format_float_str(syn_trans[stem].get('f0', ''), to_percent=False) for stem in selected_stems],
-    }
+    }, selected_stems
 
 def main():
     """Main function to generate HTML pages for audio samples."""
     env = Environment(loader=FileSystemLoader(TEMPLATE_DIR))
     template = env.get_template('page_template.html')
 
+    lang_selected_stems: dict[str, list[str]] = {}
     for page in MENU_PAGES:
         model_dir = Path(SRC_DATA_DIR) / page['src_name'] / SPLIT
         if not model_dir.exists():
             print(f"Warning: Data directory ({model_dir}) for {page['src_name']} does not exist. Skipping.")
             continue
         languages = sorted([d for d in (model_dir / 'audios').iterdir() if d.is_dir()])
-        lang_data = [get_language_data(page['src_name'], page['dst_name'], lang.name) for lang in languages]
-        lang_data = [data for data in lang_data if data is not None]  # Filter out None values
+        lang_data = [
+            get_language_data(page['src_name'], page['dst_name'], lang.name, lang_selected_stems.get(lang.name))
+            for lang in languages
+        ]
+        for lang, data in zip(languages, lang_data):
+            if data is not None and lang.name not in lang_selected_stems:
+                lang_selected_stems[lang.name] = data[1]
+        lang_data = [data[0] for data in lang_data if data is not None]  # Filter out None values
         output_html = template.render(
             page_title=page['title'],
             menu_pages=MENU_PAGES,
